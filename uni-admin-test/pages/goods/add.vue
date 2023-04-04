df4b0361345856f5355ab35a844d52553db7d023
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-04-04 17:20:32 +0800

    uni-admin-test

---
+++ uni-admin-test/pages/goods/add.vue
@@ -0,0 +1,105 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="name" label="名称">
+        <uni-easyinput placeholder="商品名称" v-model="formData.name" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="goods_desc" label="详细描述">
+        <uni-easyinput placeholder="商品详细描述" v-model="formData.goods_desc" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="goods_thumb" label="缩略图地址">
+        <uni-easyinput placeholder="商品缩略图，用于在列表或搜索结果中预览显示" v-model="formData.goods_thumb" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="remain_count" label="库存数量">
+        <uni-easyinput placeholder="库存数量" type="number" v-model="formData.remain_count"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="last_modify_date" label="">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.last_modify_date"></uni-datetime-picker>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '../../js_sdk/validator/st-goods.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'st-goods';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "name": "",
+        "goods_desc": "",
+        "goods_thumb": "",
+        "remain_count": null,
+        "last_modify_date": null
+      }
+      return {
+        formData,
+        formOptions: {},
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).add(value).then((res) => {
+          uni.showToast({
+            title: '新增成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      }
+    }
+  }
+</script>
